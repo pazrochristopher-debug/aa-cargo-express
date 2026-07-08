01e61935e946774b3eec2df1d3a88409e0414f9a
pazrochristopher-debug <pazrochristopher@gmail.com>
2026-07-08 05:03:34 -0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -6,7 +6,7 @@ const pool = new Pool({
 
 export default async function handler(req, res) {
   try {
-    const { rows } = await pool.query('SELECT * FROM packages ORDER BY id DESC');
+    const { rows } = await pool.query('SELECT * FROM packages ORDER BY created_at DESC');
     res.status(200).json({ records: rows });
   } catch (error) {
     res.status(500).json({ error: error.message });
