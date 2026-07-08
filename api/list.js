706371a6c96a04f9cc285f12b3c1a77b669161fb
pazrochristopher-debug <pazrochristopher@gmail.com>
2026-07-08 12:23:04 -0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -13,6 +13,12 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
+  
+  if (req.method!== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  // ... rest of code
+}
 
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
